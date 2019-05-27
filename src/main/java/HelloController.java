b67d0de26e20c9577114f9ae2dc80a18f1a52bcc
Internet <test@sahasra.com>
2019-05-27 12:00:32 +0100

    my sample java v0

---
+++ src/main/java/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "welcome rajesh this your sample java progream part of jenkins setup!";
+    }
+
+}
+
